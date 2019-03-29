e36d9ad8620fc385f5e5b304046a2a69cc3bfdb3
Kevin Khan <hsnks100@gmail.com>
2019-03-29 09:24:11 +0900

    Create install-script.sh

---
+++ install-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cd ~/
+git clone https://github.com/hsnks100/layervim.git
+
+echo 'set rtp+=~/layervim' >> ~/.vimrc
+echo 'source ~/layervim/layer.vim' >> ~/.vimrc
