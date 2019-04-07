cbd50851a43d509f2c70869df258b1695bdc35f5
Kevin Khan <hsnks100@gmail.com>
2019-04-08 00:04:13 +0900

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cd ~/
