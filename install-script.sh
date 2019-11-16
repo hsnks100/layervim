771f93647d8f2a3f335badfbbb977e522f4e6aa2
DIRE <hsnks100@gmail.com>
2019-11-16 11:14:24 +0900

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -2,8 +2,14 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
 cd ~/
 git clone https://github.com/hsnks100/layervim.git
 
 echo 'set rtp+=~/layervim' >> ~/.vimrc
 echo 'source ~/layervim/config.vim' >> ~/.vimrc
+
+echo 'set rtp+=~/layervim' >> ~/.config/nvim/init.vim
+echo 'source ~/layervim/config.vim' >> ~/.config/nvim/init.vim
